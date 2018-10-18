55592a8f5a7bb274de920ba10dd25a0f92225220
junhocho <junhocho@snu.ac.kr>
2018-10-18 20:18:18 +0900

    bj train?? delete checkpoints

---
+++ BJ-test_tottxt.sh
@@ -0,0 +1,7 @@
+python test.py --mask_type text-txt_bb \
+	--name total-text-txtbbmasking-random \
+	--no_flip  \
+	--dataroot /home/junho/data/Total-Text-Dataset/Dataset/totaltext/Images \
+	--phase Test \
+	--which_epoch 120 \
+	--how_many 300
---
+++ BJ-train_tottxt.sh
@@ -0,0 +1,5 @@
+python train.py --mask_type text-txt_bb-random \
+	--no_flip  \
+	--name total-text-txtbbmasking-random \
+	--dataroot /home/junho/data/Total-Text-Dataset/Dataset/totaltext/Images \
+	--phase Train
